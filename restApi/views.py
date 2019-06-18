312c129e6fd99e90b7decb8f12e4c9e70c0c0538
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-06-18 11:24:43 -0300

    POST com PUT embutido para o rest dos jogos

--- restApi/views.py
+++ restApi/views.py
@@ -13,7 +13,7 @@ class PacienteViewSet(viewsets.ModelViewSet):
 	queryset = paciente.models.Paciente.objects.all()
 	serializer_class = PacienteSerializer
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(['GET', 'POST'])
 def paciente_joga(request):
 	if request.method == 'GET':
 		if request.method == 'GET':
@@ -25,9 +25,24 @@ def paciente_joga(request):
 
 	elif request.method == 'POST':
 		serializer = PacienteJogaSerializer(data=request.data)
+		
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		if serializer.errors['non_field_errors'][0] == "Os campos nome, cpf devem criar um set único.":
+			rowJogo = paciente.models.PacienteJoga.objects.get(cpf_id=serializer.data['cpf'], nome_id=serializer.data['nome'])
+			rowJogo.tempoDeJogo += int(serializer.data['tempoDeJogo'])
+			data = {
+				"id": rowJogo.id,
+				"tempoDeJogo": rowJogo.tempoDeJogo,
+				"cpf": str(rowJogo.cpf.cpf),
+				"nome": str(rowJogo.nome),
+			}
+			new_input = PacienteJogaSerializer(rowJogo, data=data)
+			if new_input.is_valid():
+				new_input.save()
+				return Response(new_input.data, status=status.HTTP_201_CREATED)
+			return Response(new_input.errors, status=status.HTTP_400_BAD_REQUEST)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	elif request.method == 'PUT':
@@ -36,21 +51,21 @@ def paciente_joga(request):
 		print("ola")
 
 
-@api_view(['GET','POST'])
-def jogo_detail(request):
-	if request.method == 'GET':
-		if request.GET.get('cpf', '') == '':
-			return Response(status=status.HTTP_400_BAD_REQUEST)
-		pessoaJoga = paciente.models.PacienteJoga.objects.raw('SELECT id,tempoDejogo,cpf_id,nome_id FROM paciente_pacientejoga WHERE cpf_id = ' + request.GET.get('cpf', ''))
-		# pessoa = paciente.models.Paciente.objects.raw('SELECT id,tempoDejogo,cpf_id,nome_id FROM paciente_pacientejoga WHERE id = 1')
-		# + request.GET.get('cpf', '')
-		# jogos = jogos.models.Jogos.objects.all()
+# @api_view(['GET','POST'])
+# def jogo_detail(request):
+# 	if request.method == 'GET':
+# 		if request.GET.get('cpf', '') == '':
+# 			return Response(status=status.HTTP_400_BAD_REQUEST)
+# 		pessoaJoga = paciente.models.PacienteJoga.objects.raw('SELECT id,tempoDejogo,cpf_id,nome_id FROM paciente_pacientejoga WHERE cpf_id = ' + request.GET.get('cpf', ''))
+# 		# pessoa = paciente.models.Paciente.objects.raw('SELECT id,tempoDejogo,cpf_id,nome_id FROM paciente_pacientejoga WHERE id = 1')
+# 		# + request.GET.get('cpf', '')
+# 		# jogos = jogos.models.Jogos.objects.all()
 		
-		serializer = PacienteJogaSerializer(pessoaJoga, many=True)
-		return Response(serializer.data)
-	elif request.method == 'POST':
-		serializer = PacienteSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# 		serializer = PacienteJogaSerializer(pessoaJoga, many=True)
+# 		return Response(serializer.data)
+# 	elif request.method == 'POST':
+# 		serializer = PacienteSerializer(data=request.data)
+# 		if serializer.is_valid():
+# 			serializer.save()
+# 			return Response(serializer.data, status=status.HTTP_201_CREATED)
+# 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
