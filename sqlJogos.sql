9c5cc0ca6b4dd7966c0975310d136cfec407186a
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-06-18 13:33:04 -0300

    SQL para testes

--- sqlJogos.sql
+++ sqlJogos.sql
@@ -6,3 +6,8 @@ INSERT INTO jogos_jogos (nome) VALUES ("Onde Está?");
 INSERT INTO jogos_jogos (nome) VALUES ("Quanto É?");
 INSERT INTO jogos_jogos (nome) VALUES ("Quebra-Cabeça");
 INSERT INTO jogos_jogos (nome) VALUES ("Vestir");
+
+INSERT INTO paciente_pacientejoga (tempoDeJogo, cpf_id, nome_id) VALUES (60, "12345678901", "Cores");
+INSERT INTO paciente_pacientejoga (tempoDeJogo, cpf_id, nome_id) VALUES (60, "12345678902", "Memoria");
+INSERT INTO paciente_pacientejoga (tempoDeJogo, cpf_id, nome_id) VALUES (60, "12345678903", "Numeros");
+INSERT INTO paciente_pacientejoga (tempoDeJogo, cpf_id, nome_id) VALUES (60, "12345678901", "Vestir");
